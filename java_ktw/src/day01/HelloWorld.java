e254a0d3a0cd53bfc3bb630cf8af05252ef733bc
taewon1122 <tngus9773@naver.com>
2023-12-08 14:24:58 +0900

    자바 1일차 : 샘플 코드 및 설명

---
+++ java_ktw/src/day01/HelloWorld.java
@@ -0,0 +1,21 @@
+package day01;
+
+//한 줄 주석
+/*여러 줄 주석
+ * 
+ **/
+
+	public class HelloWorld {
+	
+		/**
+		 * 메소드를 설명할 때 사용하는 주석
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			//sysout + ctrl space = 콘솔에 출력할 때 사용
+			System.out.println("Hello World!"); 
+			
+			
+		}
+	
+	}
