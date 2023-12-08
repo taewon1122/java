f8d640e6d6738d55693c1c1b2782e1810a115e6d
taewon1122 <tngus9773@naver.com>
2023-12-08 17:41:55 +0900

    자바 1일차 : 연습예제

---
+++ java_ktw/src/day01/TestEx1.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// 문제1.
+		// char ch1 = "A";
+		// 문자를 저장하는 ''로 저장해야함.
+		
+		char ch1 = 'A';
+		System.out.println(ch1);
+		
+
+	}
+
+}
