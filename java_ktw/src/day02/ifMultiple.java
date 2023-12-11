db6792c3523bed5fcfcf96d2c53a586befbd7f2e
taewon1122 <tngus9773@naver.com>
2023-12-11 17:32:03 +0900

    자바 2일차 : ifmultiple 예제

---
+++ java_ktw/src/day02/ifMultiple.java
@@ -0,0 +1,26 @@
+package day02;
+
+import java.util.Scanner;
+
+public class ifMultiple {
+
+	
+	public static void main(String[] args) {
+		//정수를 입력받아 3의 배수인지 아닌지 판별하는 코드를 작성하세요.
+		//정수를 3으로 나누었을 때 나머지가 0과 같다면 3의 배수
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		int num = scan.nextInt();
+		
+		if(num % 3 == 0) {
+			System.out.println(num + "는 3의 배수입니다.");
+		}else {
+			System.out.println(num + "는 3의 배수가 아닙니다.");
+		}
+		
+		scan.close();
+		
+	}
+
+}
