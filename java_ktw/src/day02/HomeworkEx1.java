01d1ad69b3d90252f3a28ff53cfd388f33cab8d4
taewon1122 <tngus9773@naver.com>
2023-12-11 17:42:30 +0900

    자바 2일차 : Homework

--- java_ktw/src/day02/HomeworkEx1.java
+++ java_ktw/src/day02/HomeworkEx1.java
@@ -13,23 +13,37 @@ public class HomeworkEx1 {
 		 * 60미만 : F
 		 * 0보다 작거나 100보다 큰 경우 : 잘못된 점수
 		 */
-		
+	
 		Scanner scan = new Scanner(System.in);
 		System.out.print("성적을 입력하세요 : ");
 		int score = scan.nextInt();
 		
-		if(score > 100 || score < 0) {
+		if(score < 0 || score > 100) {
 			System.out.println("잘못된 점수.");
 		}else if(score < 60) {
 			System.out.println("F");
-		}else if(score >= 90 || score <= 100) {
-			System.out.println("A");
-		}else if(score >= 80 || score < 90) {
-			System.out.println("B");
-		}else if(score >= 70 || score < 80) {
-			System.out.println("C");
-		}else if(score >= 60 || score < 70) {
+		}else if(score >= 60 && score < 70) {
 			System.out.println("D");
+		}else if(score >= 70 && score < 80) {
+			System.out.println("C");
+		}else if(score >= 80 && score < 90) {
+			System.out.println("B");
+		}else if(score >= 90 && score <= 100) {
+			System.out.println("A");
+		}
+		
+		if(score < 0 || score > 100) {
+			System.out.println("잘못된 점수.");
+		}else if(score >= 90) {
+			System.out.println("A");
+		}else if(score >= 80) {
+			System.out.println("B");
+		}else if(score >= 70) {
+			System.out.println("C");
+		}else if(score >= 60) {
+			System.out.println("D");
+		}else if(score < 60) {
+			System.out.println("F");
 		}
 		
 		
