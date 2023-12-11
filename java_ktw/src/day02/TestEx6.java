ec6f24b2f14505685b54212b31b3b49ed07f719e
taewon1122 <tngus9773@naver.com>
2023-12-11 17:33:03 +0900

    자바 2일차 : TestEx6

---
+++ java_ktw/src/day02/TestEx6.java
@@ -0,0 +1,47 @@
+package day02;
+
+import java.util.Scanner;
+
+public class TestEx6 {
+
+	public static void main(String[] args) {
+		/* 산술 연산자와 두 정수를 입력받아
+		 * 산술연산자에 맞는 연산 결과를 출력하는 코드를 작성하세요.
+		 * 예1)
+		 * 두 정수와 연산자 입력 (예 : 1 + 2) : 1 + 2
+		 * 1 + 2 = 3
+		 * 예2)
+		 * 두 정수와 연산자 입력 (예 : 1 + 2) : 1 / 2
+		 * 1 / 2 = 0.5
+		 * 예1)
+		 * 두 정수와 연산자 입력 (예 : 1 + 2) : 1 ? 2
+		 * ?는 산술연산자가 아닙니다.
+		 */
+
+		//정수 입력 2개받기
+		//산술연산자 1개 입력받기 나머지 기호 예외로 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("두 정수와 연산자 입력 (예: 1 + 2) : ");
+		int num1 = scan.nextInt();
+		char operator = scan.next().charAt(0);
+		int num2 = scan.nextInt();
+		
+		if(operator == '+') {
+			System.out.println(num1 + " " + operator + " " + num2 + " = " + (num1 + num2));
+		}else if(operator == '/') {
+			System.out.println(num1 + " " + operator + " " + num2 + " = " + (num1 / (double)num2));
+		}else if(operator == '-') {
+			System.out.println(num1 + " " + operator + " " + num2 + " = " + (num1 - num2));
+		}else if(operator == '*') {
+			System.out.println(num1 + " " + operator + " " + num2 + " = " + (num1 * num2));
+		}else if(operator == '%') {
+			System.out.println(num1 + " " + operator + " " + num2 + " = " + (num1 % num2));
+		} else {
+			System.out.println("는 산술연산자가 아닙니다.");
+		}
+		scan.close();
+		
+	}
+
+}
