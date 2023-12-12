c8f11cfad653695dd944ab86f1e2f6d5079b8a8e
taewon1122 <tngus9773@naver.com>
2023-12-12 17:02:09 +0900

    자바 3일차 : Alphabet 예제 1

---
+++ java_ktw/src/day03/AlphabetEx1.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+		/* a부터 z까지 반복문을 이용하여 출력하는 코드를 작성하세요..
+		 * 반복회수	: ch는 a에서부터 z까지 1씩 증가한다.
+		 * 규칙성		: ch 출력
+		 */
+		
+		//char ch = 'a';
+		
+		for(char ch = 'a' ; ch <= 'z' ; ++ch ) {
+			System.out.print(ch);
+			
+		}
+		
+		
+		
+		
+	}
+
+}
