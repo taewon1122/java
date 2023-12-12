80af9c2a5c5c3b69bc5cf9bcbc5c6dbc34edf1e7
taewon1122 <tngus9773@naver.com>
2023-12-12 17:02:39 +0900

    자바 3일차 : For문 예제

---
+++ java_ktw/src/day03/ForEx1.java
@@ -0,0 +1,18 @@
+package day03;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		/* Hello World 5번 출력 예제
+		 * 반복횟수	: i는 1부터 5까지 1씩 증가
+		 * 규칙성		: Hello World를 출력
+		 * 반복회수는, 초기화, 조건식, 증감식을 결정
+		 */
+		int i = 5;
+		for( i = 1 ; i <= 5 ; ++i ) {
+			System.out.println("Hello world!");
+		}
+
+	}
+
+}
