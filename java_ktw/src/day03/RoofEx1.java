d53e6d4b70f6c58870199f05e82f18875987f7bb
taewon1122 <tngus9773@naver.com>
2023-12-12 17:17:00 +0900

    자바 3일차 :  입력한 정수 출력

--- java_ktw/src/day03/RoofEx1.java
+++ java_ktw/src/day03/RoofEx1.java
@@ -7,6 +7,7 @@ public class RoofEx1 {
 	public static void main(String[] args) {
 		//문자를 입력받아 입력받은 문자를 출력하고, q를 입력하면 종료하는 예제
 		Scanner scan = new Scanner(System.in);
+		// ch = 'a'를 선언한 이유는 초기화를 하지 않으면 뒤에 다른값과 비교를 할수가 없기때문에 비교를 하기 위한 아무런 값으로 선언함
 		char ch = 'a';
 		for( ; ch != 'q' ; ) {
 			System.out.print("입력 : ");
