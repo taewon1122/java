4c56790480220e68545516d0e6c607b60ed2aa75
taewon1122 <tngus9773@naver.com>
2023-12-12 21:09:29 +0900

    자바 3일차 : break 예제

--- java_ktw/src/day03/BreakEx1.java
+++ java_ktw/src/day03/BreakEx1.java
@@ -8,7 +8,7 @@ public class BreakEx1 {
 		//문자를 입력해서 입력한 문자를 출력하고, q를 입력하는 종료하는 예제
 		Scanner scan = new Scanner(System.in);
 		
-		char ch = 'a';
+		char ch = 'a'; //이거 for문에서와 똑같은 이유라서 그런건지 여쭤보기
 		while(true) {
 			System.out.print("문자입력 : ");
 			ch = scan.next().charAt(0);
