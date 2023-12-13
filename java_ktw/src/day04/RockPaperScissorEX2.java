7118483950355ac3d8be45efbf6717bf9c5533b9
taewon1122 <tngus9773@naver.com>
2023-12-13 17:54:13 +0900

    자바 4일차 : 가위바위보2

--- java_ktw/src/day04/RockPaperScissorEX2.java
+++ java_ktw/src/day04/RockPaperScissorEX2.java
@@ -16,10 +16,13 @@ public class RockPaperScissorEX2 {
 		 *  - 바위 : R, 보 : P, 가위 : S
 		 * - 기록
 		 *  - 승 무 패를 순서대로 출력
-		 */
+		 */ 
 		Scanner scan = new Scanner(System.in);
 		int menu;
-		
+		char user, com;
+		int random;
+		int R, P, S;
+		int win = 0, draw = 0, lose = 0;
 		do {
 			//메뉴 출력
 			System.out.println("-----------------");
@@ -34,15 +37,47 @@ public class RockPaperScissorEX2 {
 			//선택 메뉴 동작
 			switch(menu) {
 			case 1:
-				
+				//컴퓨터가 내는 랜덤숫자
+				random = (int)(Math.random() * 3 + 1);
+				//컴퓨터가 내는 random에서 1~3까지 r p s 선정
+				com = random == 1 ? 'R' : (random == 2 ? 'P':'S');
+				//유저 가위바위보 입력
+				System.out.println("가위(S)" + "바위(R)" + "보(P)입력 : ");
+				user = scan.next().charAt(0);
+				//사용자 rsp vs 컴퓨터 rsp 비교하고 승무패 출력
+				//비겼을 경우
+				if(com == user) {
+					System.out.println("유저 : " + user + " VS 컴퓨터 : " + com);
+					System.out.println("무승부!");
+					++draw;
+				}
+				//이겼을 경우 3가지 경우가 있음
+				else if((user == 'R' && com == 'S')
+						||(user == 'S' && com == 'P')
+						||(user == 'P' && com == 'R')) {
+					System.out.println("유저 : " + user + " VS 컴퓨터 : " + com);
+					System.out.println("유저 승!");
+					++win;
+				}
+				//졌을 경우
+				else {
+					System.out.println("유저 : " + user + " VS 컴퓨터 : " + com);
+					System.out.println("유저 패배!");
+					++lose;
+				}
+				break;
 			case 2:
-			
+				System.out.println(win + "승 "  + lose + "패 "+ draw + "무 ");
+				break;
 			case 3:
-			
-			case 4:	
+				System.out.println("프로그램을 종료합니다");
+				break;
+			default:
+				System.out.println("잘못된 메뉴입니다.");
 			}
 		}while(menu != 3);
 
+		scan.close();
 	}
 
 }
