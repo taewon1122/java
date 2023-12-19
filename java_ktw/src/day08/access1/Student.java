16d5fcc2573b30d70e71bca5df85016bce6a1ea1
taewon1122 <tngus9773@naver.com>
2023-12-19 17:29:53 +0900

    자바 8일차 : Access student

---
+++ java_ktw/src/day08/access1/Student.java
@@ -0,0 +1,36 @@
+package day08.access1;
+
+
+//public 파일이름과 class이름이 같을때 가능
+public class Student {
+	private int grade;
+	int classNum;//접근제어자가 default
+	public int num;
+	private String name;
+	
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getClassNum() {
+		return classNum;
+	}
+	public void setClassNum(int classNum) {
+		this.classNum = classNum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
