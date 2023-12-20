0774a3bfa8c0f531d79c3eeaf23dce4bdaed76a9
taewon1122 <tngus9773@naver.com>
2023-12-20 17:38:55 +0900

    자바 9일차 : SwitchEx1

---
+++ java_ktw/src/day09/SwitchEx1.java
@@ -0,0 +1,36 @@
+package day09;
+
+public class SwitchEx1 {
+
+	public static void main(String[] args) {
+		//switch문 안에 지역 변수를 선언하는 경우 발생할 수 있는 문제점
+		
+		for(int i = 0; i<5; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i<5; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		//case에서 공통적으로 사용되는 변수는 switch 위에서 선언하고 사용함.
+		int menu = 1;
+		switch(menu) {
+		case 1:
+			//여기서 선언된 num가 break문이 없으면 case 2까지 갈 수 있기 때문에
+			//여기서 선언된 num는 이후 switch문 아래코드에서 사용할 수 있음
+			int num = 10;
+			System.out.println(num);
+			break;
+		case 2:
+			//int num = 20;을 하면 위에서 선언된 num와 중복. 에러 발생
+			num = 2;
+			System.out.println(num);
+			break;
+		}
+		
+	}
+
+}
