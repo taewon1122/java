aaa9bdab88fb8105d204896a37fd3d7dafa8055e
taewon1122 <tngus9773@naver.com>
2023-12-25 23:52:29 +0900

    자바 11일차 : HomeworkEx2

--- java_ktw/src/day11/product/HomeworkEx2.java
+++ java_ktw/src/day11/product/HomeworkEx2.java
@@ -1,14 +1,57 @@
 package day11.product;
 
+import java.util.Scanner;
+
 public class HomeworkEx2 {
 	
 	/* 두 정수와 산술 연산자를 입력받아 결과를 출력하는 코드를 작성하세요.
 	 * 메서드를 생성
 	 */
 	
+	static Scanner scan = new Scanner(System.in);
+	static int a;
+	static int b;
+	static String generator;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		number();
+		generator();
+	}
 
+	private static void printAnswer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void generator() {
+		System.out.print("연산자 입력(+,-,*,/,%) : ");
+		generator = scan.next();
+		switch(generator) {
+		case "+":
+			System.out.println(a + " + " + b + " = " + (a+b));
+			break;
+		case "-":
+			System.out.println(a + " - " + b + " = " + (a-b));
+			break;
+		case "*":
+			System.out.println(a + " * " + b + " = " + (a*b));
+			break;
+		case "%":
+			System.out.println(a + " % " + b + " = " + ((float)a%b));
+			break;
+		case "/":
+			System.out.println(a + " / " + b + " = " + (a/b));
+			break;
+		default :
+			System.out.println("산술 연산자가 아님");
+			
+		}
+
+	}
+
+	private static void number() {
+		System.out.print("정수 2개 입력 : ");
+		a = scan.nextInt();
+		b = scan.nextInt();
 	}
 
 }
