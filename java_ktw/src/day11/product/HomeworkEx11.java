3ea94fdfa708cec6e4320b1bf61c90a715afbe26
taewon1122 <tngus9773@naver.com>
2023-12-26 12:48:57 +0900

    자바 11일차 : HomeworkEx11

---
+++ java_ktw/src/day11/product/HomeworkEx11.java
@@ -0,0 +1,24 @@
+package day11.product;
+
+public class HomeworkEx11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Student[] stds = new Student[5];
+	stds[0] = new Student(1, 1, 1, "홍길동");
+	stds[1] = new Student(1, 1, 2, "임꺽정");
+
+	stds[0].insertSubject("국어", 100);
+	stds[0].insertSubject("영어",50);
+	stds[1].insertSubject("수학",90);
+	stds[1].insertSubject("수학", 0);
+	
+	stds[0].print();
+	stds[1].print();
+	
+	stds[0].insertSubject("국어", 100);
+	stds[0].insertSubject("영어",50);
+	
+	}
+
+}
