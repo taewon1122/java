b3bcbde655c7bd23a156c39db8013768389ec699
taewon1122 <tngus9773@naver.com>
2024-01-02 20:20:57 +0900

    자바 16일차 : filetStreamEx2

---
+++ java_ktw/src/day16/FileStreamEx2.java
@@ -0,0 +1,36 @@
+package day16;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileStreamEx2 {
+
+	public static void main(String[] args) {
+		
+		String fileName = "src/day16/fileEx3.txt";
+		
+		//try resource를 이용한 예제로
+		//파일을 열고나서 따로 닫지 않아도 try문이 종료되면 자동으로 닫아짐
+		try(FileInputStream fis1 = new FileInputStream(fileName);
+			FileOutputStream fos1 = new FileOutputStream(fileName)) {
+			//한글출력시 엉뚱한 값이 나온다 유니코드표에 있는 문자는 정상실행 잘 안됨
+			String str = "Hello";
+			for(int i = 0; i < str.length(); i++) {
+				fos1.write(str.charAt(i));
+			}
+			for(int i = 0; i < str.length(); i++) {
+				int ch = fis1.read();
+				System.out.print((char)ch);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(fileName + " 파일을 찾을 수 없습니다.");
+		} catch (IOException e) {
+			System.out.println("파일 작업 중 예외 발생");
+		} 
+		
+	}
+
+}
