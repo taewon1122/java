5c3ef747ee1ccb2ca12cd70f15db071a50975989
taewon1122 <tngus9773@naver.com>
2024-01-05 09:11:58 +0900

    자바 18일차 : 클라이언트 클래스2

---
+++ java_ktw/src/day18/socket2/ClientEx2.java
@@ -0,0 +1,19 @@
+package day18.socket2;
+
+import java.net.Socket;
+
+public class ClientEx2 {
+
+	public static void main(String[] args) {
+		int port = 5001;
+		String ip = "192.168.30.199";
+		try {
+			Socket socket = new Socket(ip, port);
+			Client client = new Client(socket);
+			client.send();
+		} catch(Exception e) {
+			System.out.println("에외가 발생하여 종료합니다.");
+		}
+	}
+
+}
