a43afd37668fd3a7213c9b965d7b16782ac05b71
taewon1122 <tngus9773@naver.com>
2024-01-07 23:21:27 +0900

    김태원 연습1 :  WordProgram

--- java_ktw/src/ktw_test/team1/WordProgram.java
+++ java_ktw/src/ktw_test/team1/WordProgram.java
@@ -129,30 +129,70 @@ public class WordProgram implements Program{
 		//품사 입력 받는다
 		System.out.print("품사 입력 : ");
 		String wordClass = scan.next();
-		//meanList에 입력값 넣기위한 인스턴스 생성. 하기 위해 means 클래스에 생성자를 생성 한게 @data
-		
+		//meanList에 입력값 넣기위한 인스턴스 생성
 		Means means = new Means(mean,wordClass);
+		//meanList에 means 집어넣는다.
 		meanList.add(means);
+		//tmpword에 meanlist 집어넣는다
 		tmpWord.setMean(meanList);
+		//wordList에 tmpWord 집어넣는다.
 		wordList.add(tmpWord);
-		System.out.println(wordList);
+		//출력
+		for(int i = 0; i<wordList.size(); i++) {
+			System.out.println(wordList.get(i).toString());
+		}
+	}
 		
-//		
-//		if(meanList.contains(mean)) {
+		//단어 출력하는게 계속 바껴서 나오는게 입력한 tmpWord를 해서그런가
+		//[[단어 : 1[[품사] : 1 [뜻] : 1, [품사] : 2 [뜻] : 2]], [단어 : 2[[품사] : 1 [뜻] : 1, [품사] : 2 [뜻] : 2]]]
+		//이유는 단어리스트의 [0]에 품사리스트가 따로 관리되지 않아서.???
+		
+		//단어 리스트가 wordProgram에 선언되어있다. 근데 word clas
+		/*
+		 * for(int i = 0; i<wordList.size(); i++) {
+			System.out.println(wordList.get(i).toString());
+		}
+		 */
+//		이렇게 쓰니까 단어는 저장이 다 된다. 근데 품사랑 뜻이 둘다적용이되버려
+//		for(int i = 0; i<wordList.size(); i++) {
+//		System.out.println(wordList.get(i).toString() + means.toString());
+//	}
+		
+		/*
+		 * [단어 : 1][품사] : 2 [뜻] : 2
+			[단어 : 2][품사] : 2 [뜻] : 2
+		 */
+//		이거까지 해봤을때 	품사 입력하는게 mean배열에 잘 안들어갖나봐 하
+		
 //			System.out.println("이미 입력된 의미");
 //			return;
 //		}
-//		//없다면 저장
-//		meanList.add(tmpMeans);
-//		//단어 배열에다가 뜻 배열 집어넣고싶어
-//		Word tmpword = new Word(word);
-//		wordList.add(word)
-//		tmpword.printAll();
-	}
 
 	//단어 수정
 	private void updateWord() {
-
+		//단어 입력한다
+		System.out.println("단어 입력 : ");
+		String word = scan.next();
+		//word 클래스의 변수를 포함하고 있는 wordList에 입력받은 단어를 넣기 위해 인스턴스 생성
+		Word tmpWord = new Word(word);
+		//입력한 단어 있는지 확인
+		if(!wordList.contains(tmpWord)) {
+			//없으면 빠져나간다.
+			System.out.println("저장되지 않은 단어.");
+			return;
+		}else {
+			//tmpWord의 주소값을 index에 넣는다.
+			int index = wordList.indexOf(tmpWord); 
+			System.out.println("수정할 단어 입력 : ");
+			word = scan.next();
+			Word tmpWord1 = new Word(word);
+			//입력한 단어가 있다면 위치한 번지의 단어를
+			wordList.remove(index);
+			//새로운 입력값 저장
+			wordList.add(tmpWord1);
+		}
+		return;
+		//지우고
 	}
 
 	//단어 삭제
