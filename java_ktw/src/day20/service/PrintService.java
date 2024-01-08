d7f7e84228d3db1cc30e59db44615f093bee28ec
taewon1122 <tngus9773@naver.com>
2024-01-08 17:17:08 +0900

    자바 20일차 : 프린트서비스

---
+++ java_ktw/src/day20/service/PrintService.java
@@ -0,0 +1,11 @@
+package day20.service;
+
+public interface PrintService {
+
+	void printMainMenu();
+	void printWordMenu();
+	void printMeanMenu();
+	void printPrintMenu();
+	void printGameMenu();
+	
+}
