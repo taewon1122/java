4147edb3b46bc9f2295cb5050fd89c7845c4fdf1
taewon1122 <tngus9773@naver.com>
2024-01-15 08:58:26 +0900

    자바 20일차: FileServerImp

--- java_ktw/src/day20/service/FileServiceImp.java
+++ java_ktw/src/day20/service/FileServiceImp.java
@@ -12,21 +12,21 @@ public class FileServiceImp implements FileService{
 
 	@Override
 	public List<Word> load(String fileName){
-		try(ObjectInputStream ois= new ObjectInputStream(new FileInputStream(fileName))){
-			return(List<Word>) ois.readObject()
+		try(ObjectInputStream ois = 
+				new ObjectInputStream(new FileInputStream(fileName))){
+				return(List<Word>) ois.readObject();
 		} catch (Exception e) {
-			System.out.println("불러오기에 실패했습니다.");
 		}
 		return null;
 	}
 	
 	@Override
-	public boolean save(String fileName, List<Word> List) {
-		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))){
-			oos.writeObject(list);
+	public boolean save(String fileName, List<Word> list) {
+		try(ObjectOutputStream oos = 
+				new ObjectOutputStream(new FileOutputStream(fileName))){
+				oos.writeObject(list);
 			return true;
 		}catch (Exception e) {
-			
 		}
 		return false;
 	}
