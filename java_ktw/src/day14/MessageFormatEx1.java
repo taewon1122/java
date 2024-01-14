66434fb4c5c914bbac2ec93e44906355d5971e0f
taewon1122 <tngus9773@naver.com>
2024-01-15 08:57:26 +0900

    자바 14일차: 메세지포멧 예제1

--- java_ktw/src/day14/MessageFormatEx1.java
+++ java_ktw/src/day14/MessageFormatEx1.java
@@ -13,7 +13,21 @@ public class MessageFormatEx1 {
 		//MessageFormat 클래스를 이용해야함
 		String pattern = "num1 = {0}, num2 = {1}";
 		String str2 = MessageFormat.format(pattern, num1, num2);
-		System.out.println(str2);
+		
+		String d = "20230425";
+		
+		
+		
+		String year = "2023";
+		String month ="2";
+		String day = "20";
+		String p = "{0}/{1}/{2}";
+		String date = MessageFormat.format(p,year,month,day);
+		
+		//유저가 직접 2023/04/23
+		
+		//내가 정해줌 
+		System.out.println(date);
 		
 	}
 
