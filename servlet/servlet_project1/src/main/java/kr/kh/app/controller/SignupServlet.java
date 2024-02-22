0099a5a3a6af782d61a4404a61ef304ef9e1e827
taewon1122 <tngus9773@naver.com>
2024-02-22 16:50:44 +0900

    서블릿 1일차 : 회원가입, 로그인 구현완료

--- servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
@@ -1,16 +1,21 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private MemberService memberService = new MemberServiceImp();
     public SignupServlet() {
     }
 
@@ -22,8 +27,14 @@ public class SignupServlet extends HttpServlet {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String email = request.getParameter("email");
-		System.out.println(id + ", " + pw + ", " + email);
-		doGet(request, response);
+		if(memberService.signup(new MemberVO(id, pw, email))) {
+			//회원가입에 성공하면 메인 페이지로 이동
+			response.sendRedirect(request.getContextPath()+"/");
+		}
+		else {
+			//실패하면 회원가입 페이지 유지
+			doGet(request, response);
+		}
 	}
 
 }
