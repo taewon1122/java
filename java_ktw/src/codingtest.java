345734a446cd0c10c00e27f0d141c85c202c5812
taewon1122 <tngus9773@naver.com>
2024-02-27 14:11:01 +0900

    코테문제

---
+++ java_ktw/src/codingtest.java
@@ -0,0 +1,28 @@
+
+public class codingtest {
+
+	public static void main(String[] args) {
+		class Solution
+		{
+		    public int solution(int n, int a, int b)
+		    {
+		        int answer = 0;
+
+		        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		        System.out.println("Hello Java");
+
+		        return answer;
+		    }
+		}
+	}
+
+}
+/*
+ * 1번부터 n번까지 차례대로 게임을 진행
+ * 1 ,2 중 2가 이기면 2 1이 이기면 1 -> 반올림
+ * 참가자 수 n 참가자 번호 a 경쟁자번호 b
+ * 만약 a 4번 b 7번
+ * 1 2 3 (4) 5 6 (7) 8
+ * 1 (2) 3 (4) 5 6
+ * (1) (2)
+ */
