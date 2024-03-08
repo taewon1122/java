1a0a53343202d60f4028cf6e3fcfcf60a953fa61
taewon1122 <tngus9773@naver.com>
2024-03-08 12:24:10 +0900

    서블릿 11일차 : 댓글 조회 기능 구현 완료(페이지네이션 X)

---
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/CommentListServlet.java
@@ -0,0 +1,44 @@
+package kr.kh.app.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import kr.kh.app.model.vo.CommentVO;
+import kr.kh.app.pagination.CommentCriteria;
+import kr.kh.app.service.BoardService;
+import kr.kh.app.service.BoardServiceImp;
+
+@WebServlet("/comment/list")
+public class CommentListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BoardService boardService = new BoardServiceImp();
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//화면에서 보낸 현재 페이지 정보를 가져옴
+		int page = 1;
+		int boNum = 0;
+		try {
+			page = Integer.parseInt(request.getParameter("page"));
+			boNum = Integer.parseInt(request.getParameter("boNum"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		CommentCriteria cri = new CommentCriteria(page, boNum);
+		//현재 페이지에 맞는 댓글을 가져오라고 시킴
+		ArrayList<CommentVO> list = boardService.getCommentList(cri);
+		
+		JSONObject jobj = new JSONObject();
+		
+		jobj.put("list", list);
+		response.setContentType("application/json; charset=utf-8");
+		response.getWriter().print(jobj);
+	}
+}
