d453bd3d8b31d9069345b50d898ecf5eedfcd32f
taewon1122 <tngus9773@naver.com>
2024-03-12 12:35:01 +0900

    스프링 1일차 : DB연동 및 테스트 완료

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,9 +1,8 @@
 package kr.kh.spring.controller;
 
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.spring.model.dto.TestDTO;
+import kr.kh.spring.service.MemberService;
+import kr.kh.spring.service.MemberServiceImp;
 
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	private MemberService memberService;
 	
 	//value = url, method = 전송방식을 GET또는 POST
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
+		//테스트용으로 등록된 회원 수를 조회
+		int count = memberService.testCountMember();
+		System.out.println("등록된 회원 수 : " + count);
 		//model.addAttribute("화면에서 사용할 이름","보낼 데이터");
 		model.addAttribute("name","홍길동");
 		return "home";
