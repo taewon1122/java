c42c5f56cf22604bcd96160429391710b3bd0d70
taewon1122 <tngus9773@naver.com>
2024-03-13 12:20:53 +0900

    스프링 2일차 : 회원가입기능 구현 완료(비번 암호화 x)

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,18 +1,13 @@
 package kr.kh.spring.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import kr.kh.spring.model.dto.TestDTO;
 import kr.kh.spring.model.vo.MemberVO;
 import kr.kh.spring.service.MemberService;
-import kr.kh.spring.service.MemberServiceImp;
 
 @Controller
 public class HomeController {
@@ -20,30 +15,34 @@ public class HomeController {
 	@Autowired
 	private MemberService memberService;
 	
-	//value = url, method = 전송방식을 GET또는 POST
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		MemberVO member = memberService.getMember("admin");
-		System.out.println(member);
-		//model.addAttribute("화면에서 사용할 이름","보낼 데이터");
-		model.addAttribute("name","홍길동");
+		
 		return "home";
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String homePost(Model model, TestDTO testDto/*, String name, int age*/) {
-		/*System.out.println("이름 : " + name);
-		System.out.println("나이 : " + age);
-		TestDTO testDto = new TestDto(name, age);
-		*/
-		System.out.println(testDto);
-		return "home";
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String singup(Model model) {
+		
+		return "member/signup";
 	}
 	
-	@RequestMapping(value = "/test/{num}", method = RequestMethod.GET)
-	public String test(Model model, @PathVariable("num")int num) {
-		System.out.println("경로 데이터 : " + num);
-		return "redirect:/";
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String singupPost(Model model, MemberVO member) {
+		if(memberService.insertMember(member)) {
+			model.addAttribute("msg", "회원가입을 완료했습니다.");
+			model.addAttribute("url", "/");
+		}else {
+			model.addAttribute("msg", "회원가입을 하지 못했습니다.");
+			model.addAttribute("url", "/signup");
+		}
+		return "message";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		
+		return "member/login";
 	}
 	
 }
--- spring/spring/src/main/webapp/WEB-INF/views/message.jsp
+++ spring/spring/src/main/webapp/WEB-INF/views/message.jsp
@@ -1,7 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
@@ -9,22 +9,14 @@
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-	<title>스프링</title>
+	<title>메세지</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	<a class="navbar-brand" href="/">Logo</a>
-	<ul class="navbar-nav">
-		<li class="nav-item">
-			<a class="nav-link" href="<c:url value="/signup"/>">회원가입</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="<c:url value="/login"/>">로그인</a>
-		</li>
-	</ul>
-</nav>
-<div class="container">
-	<h1>메인입니다.</h1>
-</div>
+<script type="text/javascript">
+<c:if test="${msg != null}">
+	alert('${msg}');
+</c:if>
+location.href = '<c:url value="${url}"/>'
+</script>
 </body>
 </html>
